a6c9caf34a6ea323ba9577333909b1151550bf7c
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-04-14 10:59:47 -0700

    Update Attract_Mode.sh

--- Attract_Mode.sh
+++ Attract_Mode.sh
@@ -480,6 +480,6 @@ get_partun									# Download ZIP tool
 get_mbc											# Download MiSTer control tool
 build_mralist								# Generate list of MRAs
 parse_cmdline ${@}					# Parse command line parameters for input
-there_can_be_only_one ${0}	# Terminate any other running Attract Mode processes
+#there_can_be_only_one ${0}	# Terminate any other running Attract Mode processes
 loop_core										# Let Mortal Kombat begin!
 exit 1											# We should never exit here, so if we do something is wrong
