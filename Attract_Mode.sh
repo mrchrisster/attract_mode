4068c125e52907798498453f834bad069c157374
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-14 16:50:35 -0500

    Refactor to use arrays

--- Attract_Mode.sh
+++ Attract_Mode.sh
@@ -27,7 +27,7 @@
 
 # ======== DEFAULT VARIABLES ========
 # Change these in the INI file
-corelist="arcade,gba,genesis,megacd,neogeo,nes,snes,tgfx16,tgfx16cd"
+corelist="Arcade,GBA,Genesis,MegaCD,NeoGeo,NES,SNES,TGFX16,TGFX16CD"
 timer=120
 pathfs=/media/fat
 
@@ -35,9 +35,6 @@ pathfs=/media/fat
 # you can change the Path to ${pathfs}/linux for example
 mbcpath=/tmp/mbc
 partunpath=/tmp/partun
-
-# Match files case-insensitive
-#shopt -s nocasematch
 
 # ======== ARCADE OPTIONS ========
 mralist=/tmp/.Attract_Mode
@@ -48,6 +45,50 @@ orientation=All
 
 # ======== CONSOLE OPTIONS ========
 ignorezip="No"
+
+
+# ======== CORE CONFIG DATA ========
+init_data()
+{
+	# Core to file extension mappings
+	declare -gA CORE_EXT=( \
+		["arcade"]="mra" \
+		["gba"]="gba" \
+		["genesis"]="md" \
+		["megacd"]="chd" \
+		["neogeo"]="neo" \
+		["nes"]="nes" \
+		["snes"]="sfc" \
+		["tgfx16"]="pce" \
+		["tgfx16cd"]="chd" \
+		)
+	
+	# Core to path mappings
+	declare -gA CORE_PATH=( \
+		["arcade"]="${mrapath}" \
+		["gba"]="${pathfs}/games/GBA" \
+		["genesis"]="${pathfs}/games/Genesis" \
+		["megacd"]="${pathfs}/games/MegaCD" \
+		["neogeo"]="${pathfs}/games/NeoGeo" \
+		["nes"]="${pathfs}/games/NES" \
+		["snes"]="${pathfs}/games/SNES" \
+		["tgfx16"]="${pathfs}/games/TGFX16" \
+		["tgfx16cd"]="${pathfs}/games/TGFX16-CD" \
+		)
+	
+	# Core to mbc core name mappings
+	declare -gA CORE_ZIPPED=( \
+		["arcade"]="No" \
+		["gba"]="Yes" \
+		["genesis"]="Yes" \
+		["megacd"]="No" \
+		["neogeo"]="Yes" \
+		["nes"]="Yes" \
+		["snes"]="Yes" \
+		["tgfx16"]="Yes" \
+		["tgfx16cd"]="No" \
+		)
+}
 
 
 # ======== BASIC FUNCTIONS ========
@@ -78,42 +119,42 @@ parse_ini()
 parse_cmdline()
 {
 	for arg in "${@}"; do
-		case ${arg} in
+		case ${arg,,} in
 			arcade)
 				echo "MiSTer Arcade selected!"
-				declare -g corelist="arcade"
+				declare -g corelist="Arcade"
+				;;
+			gba)
+				echo "Nintendo Game Boy Advance selected!"
+				declare -g corelist="GBA"
 				;;
 			genesis)
 				echo "Sega Genesis selected!"
-				declare -g corelist="genesis"
-				;;
-			gba)
-				echo "Gameboy Advance selected!"
-				declare -g corelist="gba"
+				declare -g corelist="Genesis"
 				;;
 			megacd)
 				echo "Sega MegaCD selected!"
-				declare -g corelist="megacd"
+				declare -g corelist="MegaCD"
 				;;
 			neogeo)
 				echo "SNK NeoGeo selected!"
-				declare -g corelist="neogeo"
+				declare -g corelist="NeoGeo"
 				;;
 			nes)
 				echo "Nintendo Entertainment System selected!"
-				declare -g corelist="nes"
+				declare -g corelist="NES"
 				;;
 			snes)
 				echo "Super Nintendo Entertainment System selected!"
-				declare -g corelist="snes"
+				declare -g corelist="SNES"
 				;;
 			tgfx16cd)
 				echo "TurboGrafx-16 CD selected!"
-				declare -g corelist="tgfx16cd"
+				declare -g corelist="TGFX16CD"
 				;;
 			tgfx16)
 				echo "TurboGRAFX16 selected!"
-				declare -g corelist="tgfx16"
+				declare -g corelist="TGFX16"
 				;;
 			lucky) # Load one random core and exit with pause
 				gonext="get_lucky"
@@ -195,7 +236,7 @@ get_mbc()
 		echo "Downloading mbc - a tool needed for launching roms"
 		echo "Created for MiSTer by Pocomane"
 		echo "${REPOSITORY_URL}"
-		curl_download "${mbcpath}" "${REPOSITORY_URL}/blob/master/mbc?raw=true"
+		curl_download "${mbcpath}" "${REPOSITORY_URL}/blob/master/mbc_v02?raw=true"
 	else
 		echo "Mister Batch Control is installed, continuing..."
 	fi
@@ -227,18 +268,38 @@ loop_core()
 
 next_core()
 {
-	next=$(echo ${corelist}| xargs shuf -n1 -e)
-	next_core_${next} ${1}
+	declare -g nextcore=$(echo ${corelist}| xargs shuf -n1 -e)
+	
+	if [ "${nextcore,,}" == "arcade" ]; then
+		load_core_arcade
+		return
+	elif [ "${CORE_ZIPPED[${nextcore,,}],,}" == "yes" ]; then
+		# If not ZIP in game directory OR if ignoring ZIP
+		if [ -z "$(find ${CORE_PATH[${nextcore,,}]} -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then
+			corerom="$(find ${CORE_PATH[${nextcore,,}]} -type d \( -name *BIOS* -o -name *Other* -o -name *VGM* -o -name *NES2PCE* -o -name *FDS* -o -name *SPC* -o -name Unsupported \) -prune -false -o -name *.${CORE_EXT[${nextcore,,}]} | shuf -n 1)"
+		else # Use ZIP
+			coresh=$("${partunpath}" "$(find ${CORE_PATH[${nextcore,,}]} -maxdepth 1 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f ${CORE_EXT[${nextcore,,}]} --rename /tmp/Extracted.${CORE_EXT[${nextcore,,}]})
+			corerom="/tmp/Extracted.${CORE_EXT[${nextcore,,}]}"
+		fi
+	else
+		corerom="$(find ${CORE_PATH[${nextcore,,}]} -type f \( -iname *.${CORE_EXT[${nextcore,,}]} \) | shuf -n 1)"
+	fi
+
+	if [ -z "${corerom}" ]; then
+		core_error "${corerom}"
+	else
+		load_core "${corerom}" "${1}"
+	fi
 }
 
-load_core() 	# load_core CORE /path/to/rom (countdown)
+load_core() 	# load_core /path/to/rom (countdown)
 {	
 	echo ""
-	echo "Next up on the ${1}:"
-	echo -e "\e[1m $(echo $(basename "${2}") | sed -e 's/\.[^.]*$//') \e[0m"
-	echo "$(echo $(basename "${2}") | sed -e 's/\.[^.]*$//') (${1})" > /tmp/Attract_Game.txt
+	echo "Next up on the ${nextcore}:"
+	echo -e "\e[1m $(echo $(basename "${1}") | sed -e 's/\.[^.]*$//') \e[0m"
+	echo "$(echo $(basename "${1}") | sed -e 's/\.[^.]*$//') (${nextcore})" > /tmp/Attract_Game.txt
 
-	if [ "${3}" == "countdown" ]; then
+	if [ "${2}" == "countdown" ]; then
 		echo "Loading in..."
 		for i in {5..1}; do
 			echo "${i} seconds"
@@ -246,13 +307,13 @@ load_core() 	# load_core CORE /path/to/rom (countdown)
 		done
 	fi
 
-	"${mbcpath}" load_rom ${1} "${2}" > /dev/null 2>&1
+	"${mbcpath}" load_rom ${nextcore^^} "${1}" > /dev/null 2>&1
 }
 
-core_error() # core_error corename /path/to/ROM
+core_error() # core_error /path/to/ROM
 {
-	echo "Something went wrong! No valid game found for core ${1} - rom ${2}."
-	loop_core
+	echo "Something went wrong! No valid game found for core ${nextcore} - rom ${1}."
+	return 1
 }	
 
 # ======== LUCKY FUNCTION ========
@@ -287,7 +348,7 @@ build_mralist()
 	fi
 }
 
-next_core_arcade()
+load_core_arcade()
 {
 	# Get a random game from the list
 	mra="$(shuf -n 1 ${mralist})"
@@ -303,7 +364,7 @@ next_core_arcade()
 	# If the MRA is still not valid something is wrong - suicide
 	if [ ! -f "${mrapath}/${mra}" ]; then
 		echo "There is no valid file at ${mrapath}/${mra}!"
-		loop_core
+		return
 	fi
 
 	echo "Next up at the arcade:"
@@ -323,188 +384,15 @@ next_core_arcade()
 }
 
 
-# ======== ROM CORES ========
-# ======== GENESIS MODE (NOT PHIL) ========
-next_core_genesis()
-{
-	corename="GENESIS"
-
-	# If not ZIP in game directory OR if ignoring ZIP
-	if [ -z "$(find ${pathfs}/Games/Genesis -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then 
-		corerom="$(find ${pathfs}/Games/Genesis -type d \( -name *Eu* -o -name *BIOS* -o -name *Other* -o -name *VGM* \) -prune -false -o -name '*.md' | shuf -n 1)"
-		coresh="${corerom}"
-	else # Use ZIP
-		coresh=$("${partunpath}" "$(find ${pathfs}/Games/Genesis -maxdepth 1 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f md --rename /tmp/Genesistmp.md)
-		corerom="/tmp/Genesistmp.md"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-# ======== Gameboy Advance ========
-next_core_gba()
-{
-	corename="GBA"
-
-	# If not ZIP in game directory OR if ignoring ZIP
-	if [ -z "$(find ${pathfs}/Games/GBA -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then 
-		corerom="$(find ${pathfs}/Games/GBA -type d \( -name *Eu* -o -name *BIOS* -o -name *Other* \) -prune -false -o -name '*.gba' | shuf -n 1)"
-		coresh="${corerom}"
-	else # Use ZIP
-		coresh=$("${partunpath}" "$(find ${pathfs}/Games/GBA -maxdepth 1 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f md --rename /tmp/GBAtmp.gba)
-		corerom="/tmp/GBAtmp.gba"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
-# ======== NES MODE ========
-next_core_nes()
-{
-	corename="NES"
-
-	# If not ZIP in game directory OR if ignoring ZIP
-	if [ -z "$(find ${pathfs}/Games/NES -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then 
-		corerom="$(find ${pathfs}/Games/NES -type d \( -name *Eu* -o -name *BIOS* -o -name *Other* -o -name *FDS* \) -prune -false -o -name '*.nes' | shuf -n 1)"
-		coresh="${corerom}"
-	else # Use ZIP
-		coresh=$("${partunpath}" "$(find ${pathfs}/Games/NES -maxdepth 1 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f nes --rename /tmp/NEStmp.sfc)
-		corerom="/tmp/NEStmp.sfc"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
-# ======== SNES MODE ========
-next_core_snes()
-{
-	corename="SNES"
-
-	# If not ZIP in game directory OR if ignoring ZIP
-	if [ -z "$(find ${pathfs}/Games/SNES -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then 
-		corerom="$(find ${pathfs}/Games/SNES -type d \( -name *Eu* -o -name *BIOS* -o -name *Other* -o -name *SPC* \) -prune -false -o -name '*.sfc' | shuf -n 1)"
-		coresh="${corerom}"
-	else # Use ZIP
-		coresh=$("${partunpath}" "$(find ${pathfs}/Games/SNES -maxdepth 1 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f sfc --rename /tmp/SNEStmp.sfc)
-		corerom="/tmp/SNEStmp.sfc"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
-# ======== TGFX16 MODE ========
-next_core_tgfx16()
-{
-	corename="TGFX16"
-
-	# If not ZIP in game directory OR if ignoring ZIP
-	if [ -z "$(find ${pathfs}/Games/TGFX16 -maxdepth 1 -type f \( -iname "*.zip" \))" ] || [ "${ignorezip,,}" == "yes" ]; then 
-		corerom="$(find ${pathfs}/Games/TGFX16 -type d \( -name *Eu* -o -name *Bios* -o -name *Music* -o -name *NES2PCE* \) -prune -false -o -name '*.pce' | shuf -n 1)"
-		coresh="${corerom}"
-	else # Use ZIP
-		coresh=$("${partunpath}" "$(find ${pathfs}/Games/TGFX16 -type f \( -iname "*.zip" \) | shuf -n 1)" -i -r -f pce --rename /tmp/TGFX16tmp.pce)
-		corerom="/tmp/TGFX16tmp.pce"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-	
-
-# ======== DISC CORES ========	
-# ======== MEGA-CD MODE ========
-next_core_megacd()
-{
-	corename="MEGACD"
-
-	# Check if roms are cue or chd
-	if [ -z "$(find ${pathfs}/Games/megacd -type f \( -iname "*.chd" \))" ]; then 
-		echo "MegaCD: Roms are cue - Not supported yet"
-		loop_core
-	else # Use CHD
-		corerom="$(find ${pathfs}/Games/megacd -type f \( -iname "*.chd" \) | shuf -n 1)"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
-# ======== NEOGEO MODE ========
-next_core_neogeo()
-{
-	corename="NEOGEO"
-
-	# Check if roms are neo
-	if [ -z "$(find ${pathfs}/Games/NEOGEO -type f \( -iname "*.neo" \))" ]; then 
-		echo "NEOGEO: Not supported format, please use .neo"
-		loop_core
-	else # Use neo
-		corerom="$(find ${pathfs}/Games/NEOGEO -type f \( -iname "*.neo" \) | shuf -n 1)"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
-# ======== TGFX16-CD MODE ========
-next_core_tgfx16cd()
-{
-	corename="TURBOCD"
-	
-	# Check if roms are cue or chd
-	if [ -z "$(find ${pathfs}/Games/TGFX16-CD -type f \( -iname "*.chd" \))" ]; then 
-		echo "TGFX16-CD: Roms are cue - Not supported yet"
-		loop_core
-	else # Use CHD
-		corerom="$(find ${pathfs}/Games/TGFX16-CD -type f \( -iname "*.chd" \) | shuf -n 1)"
-	fi
-
-	if [ -z "${corerom}" ]; then
-		core_error ${corename} "${corerom}"
-	else
-		load_core ${corename} "${corerom}" "${1}"
-	fi
-}
-
-
 # ======== MAIN ========
 echo "Starting up, please wait a minute..."
-parse_ini								# Overwrite default values from INI
-curl_check								# Check network environment, configure curl
-get_partun								# Download ZIP tool
-get_mbc									# Download MiSTer control tool
+parse_ini										# Overwrite default values from INI
+curl_check									# Check network environment, configure curl
+get_partun									# Download ZIP tool
+get_mbc											# Download MiSTer control tool
 build_mralist								# Generate list of MRAs
-parse_cmdline ${@}							# Parse command line parameters for input
-there_can_be_only_one "$$" "${0}" 					# Terminate any other running Attract Mode processes
-loop_core								# Let Mortal Kombat begin!
-exit 1									# We should never exit here, so if we do something is wrong
+init_data										# Setup data arrays
+parse_cmdline ${@}					# Parse command line parameters for input
+there_can_be_only_one "$$" "${0}" # Terminate any other running Attract Mode processes
+loop_core										# Let Mortal Kombat begin!
+exit 1											# We should never exit here, so if we do something is wrong
