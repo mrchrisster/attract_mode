0774adb2eaee5fe2235c1b0ae5a83e2caa93fd4a
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-04-20 10:54:17 -0700

    no reboot

--- Attract_Mode_Autostart_Off.sh
+++ Attract_Mode_Autostart_Off.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/etc/init.d/S93attractauto stop
 mount | grep -q "on / .*[(,]ro[,$]" && RO_ROOT="true"
 [ "$RO_ROOT" == "true" ] && mount / -o remount,rw
 mv /etc/init.d/S93attractauto /etc/init.d/_S93attractauto > /dev/null 2>&1
@@ -8,7 +9,5 @@ sync
 echo "Screensaver is off and"
 echo "inactive at startup."
 echo "Done!"
-echo "Rebooting in 5s"
 sync
-sleep 5 && reboot -f
 exit 0
