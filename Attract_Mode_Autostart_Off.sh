b11fad91084efd623eddbf19df254e6be4a47899
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-20 23:01:23 -0500

    Add files via upload

--- Attract_Mode_Autostart_Off.sh
+++ Attract_Mode_Autostart_Off.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-/etc/init.d/S93attractauto stop
+/etc/init.d/S93mistersam stop
 mount | grep -q "on / .*[(,]ro[,$]" && RO_ROOT="true"
 [ "$RO_ROOT" == "true" ] && mount / -o remount,rw
-mv /etc/init.d/S93attractauto /etc/init.d/_S93attractauto > /dev/null 2>&1
+mv /etc/init.d/S93mistersam /etc/init.d/_S93mistersam > /dev/null 2>&1
 sync
 [ "$RO_ROOT" == "true" ] && mount / -o remount,ro
 
